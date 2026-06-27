b3f6051ec2ebcfb3e9e3aa426e6cc8d2408e8b86
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-06-27 16:31:22 +0300

    init project structure

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+# Put all settings user .env variables here .
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_file_encoding="utf-8")
+
+
+settings = Settings()
